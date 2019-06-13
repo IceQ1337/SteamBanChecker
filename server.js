1a0c52476a80bf0eac32e8820b2e70d8bc80cf5f
IceQ1337 <IceQ1337@users.noreply.github.com>
2019-06-13 12:06:28 +0200

    Added missing /master command
    Allows the addition of user-specific information about the script owner

--- server.js
+++ server.js
@@ -101,6 +101,8 @@ TelegramBot.on('message', (message) => {
                     sendTelegramMessage(Language.userRequestSend, chatID);
                     sendTelegramMessageAcceptDeny(`${username} ${Language.userRequestSendMaster}`, inlineKeyboard);
                 }
+            } else if (msg == '/master') {
+                sendTelegramMessage(Language.masterInfo, chatID);
             }
         }
     })
