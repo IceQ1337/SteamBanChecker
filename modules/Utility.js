0bacfcdf92e8de20829529621c3a774347fc80f9
IceQ1337 <IceQ1337@users.noreply.github.com>
2020-02-29 22:22:34 +0100

    fixed custom urls being invalid when last character is a slash

--- modules/Utility.js
+++ modules/Utility.js
@@ -28,7 +28,7 @@ module.exports = function() {
 
     this.resolveCustomURL = (customURL) => {
         return new Promise((resolve, reject) => {
-            Request(customURL + '?xml=1', (err, response, body) => {
+            Request('https://steamcommunity.com/id/' + customURL + '?xml=1', (err, response, body) => {
                 if (err) reject(err);
     
                 if (response.statusCode && response.statusCode === 200) {
@@ -48,7 +48,7 @@ module.exports = function() {
         return new Promise((resolve, reject) => {
             const profileURL = /^((http|https):\/\/(www\.)?steamcommunity.com\/profiles\/([0-9]{17}))|([0-9]{17})$/;
             const customURL = /^(http|https):\/\/(www\.)?steamcommunity.com\/id\//;
-    
+
             if (argument.match(profileURL)) {
                 var steamID = argument.match(/[0-9]{17}/gi)[0];
                 var realID = new SteamID(steamID);
@@ -58,20 +58,21 @@ module.exports = function() {
                     resolve();
                 }
             } else if (argument.match(customURL)) {
-                if (argument.replace(customURL, '').indexOf('/') == -1) {
-                    _this.resolveCustomURL(argument).then((steamID) => {
-                        var realID = new SteamID(steamID);
-                        if (realID.isValid()) {
-                            resolve(steamID);
-                        } else {
-                            resolve();
-                        }
-                    }).catch((err) => {
-                        reject(err);
-                    })
-                } else {
-                    resolve();
+                var url = argument.replace(customURL, '');
+                if (url.charAt(url.length - 1) == '/') {
+                    url = url.substring(0, url.length -1);
                 }
+
+                _this.resolveCustomURL(url).then((steamID) => {
+                    var realID = new SteamID(steamID);
+                    if (realID.isValid()) {
+                        resolve(steamID);
+                    } else {
+                        resolve();
+                    }
+                }).catch((err) => {
+                    reject(err);
+                });
             } else {
                 resolve();
             }
